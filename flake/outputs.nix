f4cc1310596439c50a3ef9b4772513f9d398b011
Jacek Generowicz <jacg@my-post-office.net>
2023-11-16 17:37:19 +0100

    Bootstrap project

---
+++ flake/outputs.nix
@@ -0,0 +1,76 @@
+{ self
+, nixpkgs # <---- This `nixpkgs` has systems removed e.g. legacyPackages.zlib
+, nain4
+, ...
+}: let
+  inherit (nixpkgs.legacyPackages) pkgs;
+
+  in {
+
+    packages.default = self.packages.crystal;
+
+    # Executed by `nix run <URL of this flake>#crystal -- <args?>`
+    # TODO: switch to clang environment
+    packages.crystal = pkgs.stdenv.mkDerivation {
+      pname = "crystal";
+      version = "0.0.0";
+      src = "${self}/src";
+      nativeBuildInputs = [];
+      buildInputs = [ nain4.packages.nain4 ];
+    };
+
+    # Executed by `nix run <URL of this flake> -- <args?>`
+    apps.default = self.apps.crystal;
+
+    # Executed by `nix run <URL of this flake>#crystal`
+    apps.crystal = let
+      g4-data = nain4.deps.g4-data-package;
+      crystal-app-package = pkgs.writeShellScriptBin "crystal" ''
+        export PATH=${pkgs.lib.makeBinPath [ self.packages.crystal ]}:$PATH
+        # export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ nain4.packages.geant4 ] }:$LD_LIBRARY_PATH
+
+        # TODO replace manual envvar setting with with use of packages' setupHooks
+        export G4NEUTRONHPDATA="${g4-data.G4NDL}/share/Geant4-11.0.4/data/G4NDL4.6"
+        export G4LEDATA="${g4-data.G4EMLOW}/share/Geant4-11.0.4/data/G4EMLOW8.0"
+        export G4LEVELGAMMADATA="${g4-data.G4PhotonEvaporation}/share/Geant4-11.0.4/data/G4PhotonEvaporation5.7"
+        export G4RADIOACTIVEDATA="${g4-data.G4RadioactiveDecay}/share/Geant4-11.0.4/data/G4RadioactiveDecay5.6"
+        export G4PARTICLEXSDATA="${g4-data.G4PARTICLEXS}/share/Geant4-11.0.4/data/G4PARTICLEXS4.0"
+        export G4PIIDATA="${g4-data.G4PII}/share/Geant4-11.0.4/data/G4PII1.3"
+        export G4REALSURFACEDATA="${g4-data.G4RealSurface}/share/Geant4-11.0.4/data/G4RealSurface2.2"
+        export G4SAIDXSDATA="${g4-data.G4SAIDDATA}/share/Geant4-11.0.4/data/G4SAIDDATA2.0"
+        export G4ABLADATA="${g4-data.G4ABLA}/share/Geant4-11.0.4/data/G4ABLA3.1"
+        export G4INCLDATA="${g4-data.G4INCL}/share/Geant4-11.0.4/data/G4INCL1.0"
+        export G4ENSDFSTATEDATA="${g4-data.G4ENSDFSTATE}/share/Geant4-11.0.4/data/G4ENSDFSTATE2.3"
+
+        exec crystal --macro-path ${self}/macs "$@"
+      '';
+    in { type = "app"; program = "${crystal-app-package}/bin/crystal"; };
+
+
+    # Used by `direnv` when entering this directory (also by `nix develop <URL to this flake>`)
+    devShell = self.devShells.clang;
+
+    # Activated by `nix develop <URL to this flake>#clang`
+    devShells.clang = pkgs.mkShell.override { stdenv = nain4.packages.clang_16.stdenv; } {
+      name = "crystal-clang-devenv";
+      packages = nain4.deps.dev-shell-packages ++ [ nain4.packages.clang_16 ];
+    };
+
+    # Activated by `nix develop <URL to this flake>#gcc`
+    devShells.gcc = pkgs.mkShell {
+      name = "crystal-gcc-devenv";
+      packages = nain4.deps.dev-shell-packages;
+    };
+
+    # 1. `nix build` .#singularity
+    # 2. `scp result <me>@lxplus7.cern.ch:hello.img`
+    # 3. [on lxplus] `singularity run hello.img`
+    packages.singularity = pkgs.singularity-tools.buildImage {
+      name = "crystal";
+      contents = [ self.apps.crystal.program ];
+      runScript = "${self.apps.crystal.program} $@";
+      diskSize = 10240;
+      memSize = 5120;
+    };
+
+  }
---
+++ justfile
@@ -0,0 +1,35 @@
+# -*-Makefile-*-
+
+# Needed to make `"$@"` usable in recipes
+set positional-arguments := true
+
+default:
+  just run --beam-on 10
+
+test *REST:
+  meson setup build/test test
+  meson compile -C build/test
+  meson install -C build/test
+  sh install/test/run-each-test-in-separate-process.sh "$@"
+
+catch2-demo *REST:
+  echo "$@"
+  meson setup build/test test
+  meson compile -C build/test
+  meson install -C build/test
+  install/test/bin/catch2-demo-test "$@"
+
+build:
+  meson setup build/app src
+  meson compile -C build/app
+
+install: build
+  meson install -C build/app
+
+run *ARGS: install
+  #!/usr/bin/env sh
+  sh execute-with-nixgl-if-needed.sh ./install/app/bin/crystal "$@"
+  exit $?
+
+clean:
+  rm build install -rf
