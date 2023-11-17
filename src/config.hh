554f7a9757ef152515baf3c8ad1c42ddeffd8ea9
Jacek Generowicz <jacg@my-post-office.net>
2023-11-17 15:04:17 +0100

    Add random seed setter to messenger

--- src/config.hh
+++ src/config.hh
@@ -3,6 +3,7 @@
 #include <G4GenericMessenger.hh>
 #include <G4SystemOfUnits.hh>
 #include <G4UnitsTable.hh>
+#include <Randomize.hh>
 
 
 enum class scintillator_type { lyso, bgo, csi };
@@ -19,8 +20,8 @@ struct config {
   G4double          particle_energy    {511    * keV};
   G4double          source_pos         {-50    * mm};
   G4double          scint_yield        {50000    / MeV};
+  G4long            seed               {123456789};
 
-  void set_scint(const std::string& s) { scintillator_type = string_to_scintillator_type(s); }
   config()
   // The trailing slash after '/my_geometry' is CRUCIAL: without it, the
   // messenger violates the principle of least surprise.
@@ -30,15 +31,20 @@ struct config {
 
     new G4UnitDefinition("1/MeV","1/MeV", "1/Energy", 1/MeV);
 
-    msg -> DeclareMethod          ("scint_type"          ,       &config::set_scint  );
-    msg -> DeclareProperty        ("scint_size"          ,        scint_size         );
-    msg -> DeclarePropertyWithUnit("scint_yield", "1/MeV",        scint_yield        );
-    msg -> DeclareProperty        ("reflector_thickness" ,        reflector_thickness);
-    msg -> DeclarePropertyWithUnit("particle_energy"     , "keV", particle_energy    );
-    msg -> DeclareProperty        ("physics_verbosity"   ,        physics_verbosity  );
-    msg -> DeclareProperty        ("source_pos"          ,        source_pos         );
+    msg -> DeclareMethod          ("scint_type"          ,       &config::set_scint      );
+    msg -> DeclareProperty        ("scint_size"          ,        scint_size             );
+    msg -> DeclarePropertyWithUnit("scint_yield", "1/MeV",        scint_yield            );
+    msg -> DeclareProperty        ("reflector_thickness" ,        reflector_thickness    );
+    msg -> DeclarePropertyWithUnit("particle_energy"     , "keV", particle_energy        );
+    msg -> DeclareProperty        ("physics_verbosity"   ,        physics_verbosity      );
+    msg -> DeclareProperty        ("source_pos"          ,        source_pos             );
+    msg -> DeclareMethod          ("seed"                ,       &config::set_random_seed);
+
+    set_random_seed(seed);
   }
 private:
+  void set_scint(const std::string& s) { scintillator_type = string_to_scintillator_type(s); }
+  void set_random_seed(G4long seed) { G4Random::setTheSeed(seed); }
   G4GenericMessenger* msg;
 };
 
