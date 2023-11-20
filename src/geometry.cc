37473a3f7970c69ee89221aa580671b2277c3063
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-11-20 17:28:56 +0200

    Add LogicalBorderSurface between scintillator and reflector

--- src/geometry.cc
+++ src/geometry.cc
@@ -5,6 +5,9 @@
 #include <n4-material.hh>
 #include <n4-shape.hh>
 
+#include <G4OpticalSurface.hh>
+#include <G4LogicalBorderSurface.hh>
+
 std::tuple<G4double, G4double, G4double> unpack(const G4ThreeVector& v) { return {v.x(), v.y(), v.z()}; }
 
 G4PVPlacement* crystal_geometry() {
@@ -22,10 +25,21 @@ G4PVPlacement* crystal_geometry() {
     .place(teflon).at_z(-(sz + my.reflector_thickness) / 2)
     .in(world).now();
 
-  n4::box("crystal")
+  auto crystal = n4::box("crystal")
     .xyz(sx, sy, sz)
     .place(scintillator).at_z(my.reflector_thickness / 2)
     .in(reflector).now();
+
+  auto
+  teflon_surface = new G4OpticalSurface("teflon_surface");
+  teflon_surface -> SetType(dielectric_dielectric);
+  teflon_surface -> SetModel(unified);
+  teflon_surface -> SetFinish(groundfrontpainted);
+  teflon_surface -> SetSigmaAlpha(0.0);
+
+  teflon_surface -> SetMaterialPropertiesTable(teflon_properties());
+  new G4LogicalBorderSurface("teflon_surface", crystal, reflector, teflon_surface);
+
 
   return world;
 }
