271d601209f1fbdcd0b9593bc4c542717eaa31be
Jacek Generowicz <jacg@my-post-office.net>
2023-11-23 08:53:56 +0100

    Fix size of sipm

--- src/geometry.cc
+++ src/geometry.cc
@@ -42,8 +42,10 @@ G4PVPlacement* crystal_geometry(unsigned& n_detected_evt) {
 
   auto sipm_detector = new n4::sensitive_detector("sipm", process_hits);
 
+  auto Nx = my.scint_params.n_sipms_x;
+  auto Ny = my.scint_params.n_sipms_y;
   auto sipm = n4::box("sipm")
-    .xyz(sx, sy, sipm_thickness)
+    .xyz(sx/Nx, sy/Ny, sipm_thickness)
     .sensitive(sipm_detector)
     .place(silicon).in(world);
 
