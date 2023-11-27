d96de154dec94b993e5f94bd327eb30ad7acfa10
Jacek Generowicz <jacg@my-post-office.net>
2023-11-27 15:15:09 +0100

    Add and use abstraction for making nix flake apps

--- flake/outputs.nix
+++ flake/outputs.nix
@@ -15,6 +15,32 @@
       '';
     };
 
+  make-app = executable: args:
+    let
+      g4-data = nain4.deps.g4-data-package;
+      crystal-app-package = pkgs.writeShellScriptBin executable ''
+        export PATH=${pkgs.lib.makeBinPath [ self.packages.crystal ]}:$PATH
+        # export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ nain4.packages.geant4 ] }:$LD_LIBRARY_PATH
+
+        # TODO replace manual envvar setting with with use of packages' setupHooks
+        export G4NEUTRONHPDATA="${g4-data.G4NDL}/share/Geant4-11.0.4/data/G4NDL4.6"
+        export G4LEDATA="${g4-data.G4EMLOW}/share/Geant4-11.0.4/data/G4EMLOW8.0"
+        export G4LEVELGAMMADATA="${g4-data.G4PhotonEvaporation}/share/Geant4-11.0.4/data/G4PhotonEvaporation5.7"
+        export G4RADIOACTIVEDATA="${g4-data.G4RadioactiveDecay}/share/Geant4-11.0.4/data/G4RadioactiveDecay5.6"
+        export G4PARTICLEXSDATA="${g4-data.G4PARTICLEXS}/share/Geant4-11.0.4/data/G4PARTICLEXS4.0"
+        export G4PIIDATA="${g4-data.G4PII}/share/Geant4-11.0.4/data/G4PII1.3"
+        export G4REALSURFACEDATA="${g4-data.G4RealSurface}/share/Geant4-11.0.4/data/G4RealSurface2.2"
+        export G4SAIDXSDATA="${g4-data.G4SAIDDATA}/share/Geant4-11.0.4/data/G4SAIDDATA2.0"
+        export G4ABLADATA="${g4-data.G4ABLA}/share/Geant4-11.0.4/data/G4ABLA3.1"
+        export G4INCLDATA="${g4-data.G4INCL}/share/Geant4-11.0.4/data/G4INCL1.0"
+        export G4ENSDFSTATEDATA="${g4-data.G4ENSDFSTATE}/share/Geant4-11.0.4/data/G4ENSDFSTATE2.3"
+
+        exec ${executable} ${args}
+      '';
+    in { type = "app"; program = "${crystal-app-package}/bin/${executable}"; };
+
+  args-from-cli = ''"$@"'';
+
   in {
 
     packages.default = self.packages.crystal;
@@ -30,54 +56,8 @@
     # Executed by `nix run <URL of this flake> -- <args?>`
     apps.default = self.apps.crystal;
 
-    # Executed by `nix run <URL of this flake>#crystal`
-    apps.crystal = let
-      g4-data = nain4.deps.g4-data-package;
-      crystal-app-package = pkgs.writeShellScriptBin "crystal" ''
-        export PATH=${pkgs.lib.makeBinPath [ self.packages.crystal ]}:$PATH
-        # export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ nain4.packages.geant4 ] }:$LD_LIBRARY_PATH
-
-        # TODO replace manual envvar setting with with use of packages' setupHooks
-        export G4NEUTRONHPDATA="${g4-data.G4NDL}/share/Geant4-11.0.4/data/G4NDL4.6"
-        export G4LEDATA="${g4-data.G4EMLOW}/share/Geant4-11.0.4/data/G4EMLOW8.0"
-        export G4LEVELGAMMADATA="${g4-data.G4PhotonEvaporation}/share/Geant4-11.0.4/data/G4PhotonEvaporation5.7"
-        export G4RADIOACTIVEDATA="${g4-data.G4RadioactiveDecay}/share/Geant4-11.0.4/data/G4RadioactiveDecay5.6"
-        export G4PARTICLEXSDATA="${g4-data.G4PARTICLEXS}/share/Geant4-11.0.4/data/G4PARTICLEXS4.0"
-        export G4PIIDATA="${g4-data.G4PII}/share/Geant4-11.0.4/data/G4PII1.3"
-        export G4REALSURFACEDATA="${g4-data.G4RealSurface}/share/Geant4-11.0.4/data/G4RealSurface2.2"
-        export G4SAIDXSDATA="${g4-data.G4SAIDDATA}/share/Geant4-11.0.4/data/G4SAIDDATA2.0"
-        export G4ABLADATA="${g4-data.G4ABLA}/share/Geant4-11.0.4/data/G4ABLA3.1"
-        export G4INCLDATA="${g4-data.G4INCL}/share/Geant4-11.0.4/data/G4INCL1.0"
-        export G4ENSDFSTATEDATA="${g4-data.G4ENSDFSTATE}/share/Geant4-11.0.4/data/G4ENSDFSTATE2.3"
-
-        exec crystal --macro-path ${self}/macs "$@"
-      '';
-    in { type = "app"; program = "${crystal-app-package}/bin/crystal"; };
-
-    # Executed by `nix run <URL of this flake>#stats`
-    apps.stats = let
-      g4-data = nain4.deps.g4-data-package;
-      crystal-app-package = pkgs.writeShellScriptBin "stats" ''
-        export PATH=${pkgs.lib.makeBinPath [ self.packages.crystal ]}:$PATH
-        # export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ nain4.packages.geant4 ] }:$LD_LIBRARY_PATH
-
-        # TODO replace manual envvar setting with with use of packages' setupHooks
-        export G4NEUTRONHPDATA="${g4-data.G4NDL}/share/Geant4-11.0.4/data/G4NDL4.6"
-        export G4LEDATA="${g4-data.G4EMLOW}/share/Geant4-11.0.4/data/G4EMLOW8.0"
-        export G4LEVELGAMMADATA="${g4-data.G4PhotonEvaporation}/share/Geant4-11.0.4/data/G4PhotonEvaporation5.7"
-        export G4RADIOACTIVEDATA="${g4-data.G4RadioactiveDecay}/share/Geant4-11.0.4/data/G4RadioactiveDecay5.6"
-        export G4PARTICLEXSDATA="${g4-data.G4PARTICLEXS}/share/Geant4-11.0.4/data/G4PARTICLEXS4.0"
-        export G4PIIDATA="${g4-data.G4PII}/share/Geant4-11.0.4/data/G4PII1.3"
-        export G4REALSURFACEDATA="${g4-data.G4RealSurface}/share/Geant4-11.0.4/data/G4RealSurface2.2"
-        export G4SAIDXSDATA="${g4-data.G4SAIDDATA}/share/Geant4-11.0.4/data/G4SAIDDATA2.0"
-        export G4ABLADATA="${g4-data.G4ABLA}/share/Geant4-11.0.4/data/G4ABLA3.1"
-        export G4INCLDATA="${g4-data.G4INCL}/share/Geant4-11.0.4/data/G4INCL1.0"
-        export G4ENSDFSTATEDATA="${g4-data.G4ENSDFSTATE}/share/Geant4-11.0.4/data/G4ENSDFSTATE2.3"
-
-        exec stats "$@"
-      '';
-    in { type = "app"; program = "${crystal-app-package}/bin/stats"; };
-
+    apps.crystal = make-app "crystal" "--macro-path ${self}/macs ${args-from-cli}";
+    apps.stats   = make-app "stats"                                args-from-cli;
 
     # Used by `direnv` when entering this directory (also by `nix develop <URL to this flake>`)
     devShell = self.devShells.clang;
