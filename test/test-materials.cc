12b00e8f97807ddbb7668a8db180a5919e9feb07
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-12-19 16:04:07 +0200

    Add test for sipm pde

--- test/test-materials.cc
+++ test/test-materials.cc
@@ -46,6 +46,20 @@ auto blue_light_towards_teflon() {
   };
 }
 
+
+auto photons_along_z(auto energy) {
+  auto particle_type = n4::find_particle("opticalphoton");
+  auto source_z      = - my.scint_size().z() / 2;
+  auto isotropic     = n4::random::direction{};
+  return [energy, source_z, particle_type, isotropic] (G4Event* event) {
+    auto particle = new G4PrimaryParticle{particle_type, 0, 0, energy};
+    auto vertex   = new G4PrimaryVertex{{0,0,source_z}, 0};
+    particle -> SetPolarization(isotropic.get());
+    vertex -> SetPrimary(particle);
+    event  -> AddPrimaryVertex(vertex);
+  };
+}
+
 // Check whether step exists and if so, whether its value is as specified
 bool step_matches(std::vector<G4LogicalVolume*>& step_volumes, size_t pos, G4LogicalVolume* value) {
     return step_volumes.size() > pos && step_volumes[pos] == value;
@@ -289,4 +303,23 @@ TEST_CASE("lyso interaction process fractions", "[lyso][interaction]") {
   CHECK_THAT(fractions.photoelectric, WithinRel(0.311, 2e-2));
   CHECK_THAT(fractions.compton      , WithinRel(0.637, 2e-2));
   CHECK_THAT(fractions.rayleigh     , WithinRel(0.051, 2e-2));
+}
+
+TEST_CASE("sipm_pde", "[sipm][pde]") {
+  run_stats stats;
+
+  auto N      = 1'000'000;
+  auto energy = 3.5 * eV;
+  auto [pde_energies, pde_values] = sipm_pde();
+  auto pde_at_energy = n4::interpolator(std::move(pde_energies), std::move(pde_values))(energy).value();
+  n4::run_manager::create()
+    .fake_ui()
+    .physics(physics_list)
+    .geometry([&] {return crystal_geometry(stats);})
+    .actions(new n4::actions{photons_along_z(energy)})
+    .run(N);
+
+  auto fraction_detected = static_cast<double>(stats.n_detected_at_sipm[0]) / N;
+  CHECK_THAT(fraction_detected, WithinRel(pde_at_energy, 1e-2));
+
 }
