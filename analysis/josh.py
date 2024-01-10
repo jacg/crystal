ec191ba2f7ccfe36411730306eb573c2e7c79faf
Jacek Generowicz <jacg@my-post-office.net>
2024-01-10 12:24:17 +0100

    Add option to suppress interactive plot
    [skip ci]

--- analysis/josh.py
+++ analysis/josh.py
@@ -29,7 +29,8 @@ class HackClick(Exception):
 @click.option("--epochs", default=10, help="Number of epoch to use during training")
 @click.option("--fig-filename", default='figs.png', help="Filename where figures should be written")
 @click.option("--data-type", default='parquet', help="Type of data to read (parquet | csv-npy)")
-def parse_args(max_files, plot_event, data_dir, epochs, fig_filename, data_type):
+@click.option("--no-interactive-plot", is_flag=True, show_default=True, default=False, help="Suppress interactive display of results")
+def parse_args(max_files, plot_event, data_dir, epochs, fig_filename, data_type, no_interactive_plot):
     """Train neural net from data in parquet files."""
     opts['data_dir']     = data_dir
     opts['max_files']    = max_files
@@ -37,6 +38,7 @@ def parse_args(max_files, plot_event, data_dir, epochs, fig_filename, data_type)
     opts['epochs']       = epochs
     opts['fig_filename'] = fig_filename
     opts['data_type']    = data_type
+    opts['no_plot']      = no_interactive_plot
     raise HackClick
 
 try:
@@ -338,19 +340,21 @@ ax0, ax1 = flat_axes[0], flat_axes[1]
 
 print(f"NN: σx {np.std(delta_x_NN):.2f}  σy {np.std(delta_y_NN):.2f}  σz {np.std(delta_z_NN):.2f}   classical: σx {np.std(delta_x_classical):.2f}  σy {np.std(delta_y_classical):.2f}")
 
-ax0.hist(delta_x_NN, bins=nbins, label=f"x ($\sigma$ = {np.std(delta_x_NN):.2f})", alpha=0.7)
-ax0.hist(delta_y_NN, bins=nbins, label=f"y ($\sigma$ = {np.std(delta_y_NN):.2f})", alpha=0.7)
-ax0.hist(delta_z_NN, bins=nbins, label=f"z ($\sigma$ = {np.std(delta_z_NN):.2f})", alpha=0.7)
-ax0.set_xlabel("NN (True - Predicted) Positions",fontsize=14)
-ax0.set_ylabel("Counts/bin",fontsize=14)
-ax0.legend()
+if not opts["no_plot"]:
 
-ax1.hist(delta_x_classical, bins=nbins, label=f"x ($\sigma$ = {np.std(delta_x_classical):.2f})", alpha=0.7)
-ax1.hist(delta_y_classical, bins=nbins, label=f"y ($\sigma$ = {np.std(delta_y_classical):.2f})", alpha=0.7)
-ax1.set_xlabel("Classical (True - Predicted) Positions",fontsize=14)
-ax1.set_ylabel("Counts/bin",fontsize=14)
-ax1.legend()
+    ax0.hist(delta_x_NN, bins=nbins, label=f"x ($\sigma$ = {np.std(delta_x_NN):.2f})", alpha=0.7)
+    ax0.hist(delta_y_NN, bins=nbins, label=f"y ($\sigma$ = {np.std(delta_y_NN):.2f})", alpha=0.7)
+    ax0.hist(delta_z_NN, bins=nbins, label=f"z ($\sigma$ = {np.std(delta_z_NN):.2f})", alpha=0.7)
+    ax0.set_xlabel("NN (True - Predicted) Positions",fontsize=14)
+    ax0.set_ylabel("Counts/bin",fontsize=14)
+    ax0.legend()
 
-plt.suptitle(f'{opts["data_dir"]} - {INPUT_TYPE}')
-plt.savefig(opts['fig_filename'])
-plt.show()
+    ax1.hist(delta_x_classical, bins=nbins, label=f"x ($\sigma$ = {np.std(delta_x_classical):.2f})", alpha=0.7)
+    ax1.hist(delta_y_classical, bins=nbins, label=f"y ($\sigma$ = {np.std(delta_y_classical):.2f})", alpha=0.7)
+    ax1.set_xlabel("Classical (True - Predicted) Positions",fontsize=14)
+    ax1.set_ylabel("Counts/bin",fontsize=14)
+    ax1.legend()
+
+    plt.suptitle(f'{opts["data_dir"]} - {INPUT_TYPE}')
+    plt.savefig(opts['fig_filename'])
+    plt.show()
